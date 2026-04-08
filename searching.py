379b4c7be9196fa9844c0159e36acc5f3a1f1f84
Richard Kovalsky <kvl.richard@gmail.com>
2026-04-08 09:24:37 +0200

    Commit3

--- searching.py
+++ searching.py
@@ -1,5 +1,7 @@
 import json
 from pathlib import Path
+from traceback import walk_tb
+
 
 # get current working directory path
 
@@ -20,11 +22,30 @@ def read_data(file_name, field):
                 return value
         return None
 
+def linear_search(seq,wanted):
+    search = {}
+    positions = []
+    count = 0
+    for idx,n in enumerate(seq):
+        if n == wanted:
+            positions.append(idx)
+            count += 1
+    search = {"positions":positions,"count":count}
+    return search
+
+
+def binary_search(numbers,wanted_number):
+
 
 
 def main():
-    sequential_data = read_data("sequential.json","unordered_numbers")
+    sequence = read_data("sequential.json","dna_sequence")
+    wanted_NK = linear_search(sequence,"A")
+
+    numbers = read_data("sequential.json","ordered_numbers")
+
     print(sequential_data)
+    print(wanted_NK)
 
 
 if __name__ == '__main__':
